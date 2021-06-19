b77e6be2273fa9db752265d9b7bdd037b11e4fbe
Naveen <naveen11233@gmail.com>
2021-06-19 10:27:36 +0530

    Inside develop/naveen new try

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Valentine Animation On Develop',
+      title: 'Valentine Animation On Develop Inside Naveen',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
